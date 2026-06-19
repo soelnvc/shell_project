ffa5f900e2ad1e9f683064543fde7880fddde5e4
Siddhesh Goel <soelnvc@gmail.com>
2026-06-19 16:00:24 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-
         while(true) {
             System.out.print("$ ");
-
+            
             String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
             System.out.println(input + ": command not found");
         }
     }
